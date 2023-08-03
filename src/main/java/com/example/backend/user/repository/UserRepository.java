4746e0f4e699f6d034d02b338db6e5314aedb2e9
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-03 21:22:04 +0900

    style : 스타일 수정

--- src/main/java/com/example/backend/user/repository/UserRepository.java
+++ src/main/java/com/example/backend/user/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 	Optional<User> findByEmail(String email);
-
 	Optional<User> findByKakaoId(Long kakaoId);
 	Optional<User> findByGoogleId(Long googleId);
 }
