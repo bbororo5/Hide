f38aab1e38a2c8c45052a099a28a2d2e6d4699da
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-04 23:55:47 +0900

    refactor : cors관련

---
+++ src/main/java/com/example/backend/security/CorsFilter.java
@@ -0,0 +1,42 @@
+package com.example.backend.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    //    CORS 설정 필터
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String[] urlList = {"http://localhost:3000", "헤헤헤"};
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Expose-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- src/main/java/com/example/backend/security/JwtAuthorizationFilter.java
+++ src/main/java/com/example/backend/security/JwtAuthorizationFilter.java
@@ -1,7 +1,12 @@
 package com.example.backend.security;
 
-import java.io.IOException;
-
+import com.example.backend.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -10,60 +15,56 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.example.backend.util.JwtUtil;
-
-import io.jsonwebtoken.Claims;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
 
 @Slf4j(topic = "Jwt Authorization")
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
-	private final JwtUtil jwtUtil;
-	private final UserDetailsServiceImpl serviceImpl;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl serviceImpl;
 
-	public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl serviceImpl) {
-		this.jwtUtil = jwtUtil;
-		this.serviceImpl = serviceImpl;
-	}
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl serviceImpl) {
+        this.jwtUtil = jwtUtil;
+        this.serviceImpl = serviceImpl;
+    }
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain) throws
-		ServletException,
-		IOException {
-		String token = request.getHeader(JwtUtil.AUTHORIZATION_HEADER);
-		if (StringUtils.hasText(token)) {
-			token = jwtUtil.substringToken(token);
-			if (!jwtUtil.validateToken(token)) {
-				log.error("유효성 검증에 실패했습니다.");
-				return;
-			}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws
+            ServletException,
+            IOException {
+        String token = request.getHeader(JwtUtil.AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(token)) {
+            if (token.startsWith(JwtUtil.BEARER_PREFIX)) {
+                token = jwtUtil.substringToken(token);
+            }
 
-			Claims claims = jwtUtil.getUserInfoFromToken(token);
-			String email = claims.getSubject();
-			log.info(email);
-			try {
-				setAuthentication(email);
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
+            if (!jwtUtil.validateToken(token)) {
+                log.error("유효성 검증에 실패했습니다.");
+                return;
+            }
 
-		filterChain.doFilter(request, response);
-	}
+            Claims claims = jwtUtil.getUserInfoFromToken(token);
+            String email = claims.getSubject();
+            log.info(email);
+            try {
+                setAuthentication(email);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
-	public void setAuthentication(String email) {
-		SecurityContext context = SecurityContextHolder.createEmptyContext();
-		Authentication auth = createAuthentication(email);
-		context.setAuthentication(auth);
-		SecurityContextHolder.setContext(context);
-	}
+        filterChain.doFilter(request, response);
+    }
 
-	private Authentication createAuthentication(String email) {
-		UserDetails userDetails = serviceImpl.loadUserByUsername(email);
-		return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-	}
+    public void setAuthentication(String email) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication auth = createAuthentication(email);
+        context.setAuthentication(auth);
+        SecurityContextHolder.setContext(context);
+    }
+
+    private Authentication createAuthentication(String email) {
+        UserDetails userDetails = serviceImpl.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 }
