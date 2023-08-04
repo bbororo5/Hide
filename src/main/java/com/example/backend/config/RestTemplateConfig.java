f17f5ce24f09d67dfd1d98f64f40d24e28c66f09
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-04 11:56:27 +0900

    style

--- src/main/java/com/example/backend/config/RestTemplateConfig.java
+++ src/main/java/com/example/backend/config/RestTemplateConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder
