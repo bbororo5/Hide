e150f2c7c82ef14957fd8810975cda919a900456
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-16 09:28:34 +0900

    Refactor : origin main pull

--- src/main/java/com/example/backend/playlist/controller/PlayListController.java
+++ src/main/java/com/example/backend/playlist/controller/PlayListController.java
@@ -9,17 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.StatusResponseDto;
 import com.example.backend.playlist.dto.PlaylistDto;
-import com.example.backend.playlist.entity.Playlist;
 import com.example.backend.playlist.service.PlayListService;
 import com.example.backend.util.security.UserDetailsImpl;
-import com.example.backend.util.spotify.dto.Track;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
