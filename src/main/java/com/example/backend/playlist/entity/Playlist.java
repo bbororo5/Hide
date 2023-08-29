b430ea88c6b6a63d766bae7a8abecd8a474da79f
algorijun <123007169+HGive@users.noreply.github.com>
2023-08-29 14:19:20 +0900

    Refactor : playlist fetchType

--- src/main/java/com/example/backend/playlist/entity/Playlist.java
+++ src/main/java/com/example/backend/playlist/entity/Playlist.java
@@ -10,6 +10,7 @@ import com.example.backend.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,7 +35,7 @@ public class Playlist {
 	@Column(updatable = false)
 	private LocalDateTime createdAt;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	User user;
 
 	public Playlist(String trackId, User user) {
